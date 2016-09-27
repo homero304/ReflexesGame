89fd8bff41f59205bb67ee26eccc1179ed4cd38c
Homero Enrique Marin Galindo <homeromaring@gmail.com>
2016-09-27 12:37:34 -0500

    timer count down
    en espaniol

--- ReflexesGame/ReflexesGame/MainWindow.xaml.cs
+++ ReflexesGame/ReflexesGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ReflexesGame
 {
@@ -20,9 +21,37 @@ namespace ReflexesGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        int timeLeft = 60;
+        DispatcherTimer timer;
+        Random randomize = new Random();
         public MainWindow()
         {
             InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000);
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.IsEnabled = true;
+        }
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                timerLabel.Content = "Te quedan " + timeLeft + " segundos";
+            }
+            else
+            {
+                timer.Stop();
+                timerLabel.Content = "Se acabo del tiempo!";
+            }
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
         }
     }
 }
